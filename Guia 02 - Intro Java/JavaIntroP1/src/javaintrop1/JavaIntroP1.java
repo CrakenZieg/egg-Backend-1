ed4e808ffdf87d0c999dd16b0b502f3f35c31930
Francisco Ziegler <franciscoziegler@gmail.com>
2023-03-09 19:32:32 -0300

    retoque con menu incluido

--- Guia 02 - Intro Java/JavaIntroP1/src/javaintrop1/JavaIntroP1.java
+++ Guia 02 - Intro Java/JavaIntroP1/src/javaintrop1/JavaIntroP1.java
@@ -2,29 +2,50 @@
 package javaintrop1;
 
 import ejercicios.*;
+import java.util.Scanner;
 
 public class JavaIntroP1 {
 
     public static void main(String[] args) {
-        
-//        TeoriaEjercicios teoriaEjercicios = new TeoriaEjercicios();
-//        teoriaEjercicios.ejercicio();
-    
-//        Ejercicio1 ejercicio1 = new Ejercicio1();
-//        ejercicio1.ejercicio();
-
-//        Ejercicio2 ejercicio2 = new Ejercicio2();
-//        ejercicio2.ejercicio();
-
-//        Ejercicio3 ejercicio3 = new Ejercicio3();
-//        ejercicio3.ejercicio();
-
-//        Ejercicio4 ejercicio4 = new Ejercicio4();
-//        ejercicio4.ejercicio();
-
-//        Ejercicio5 ejercicio5 = new Ejercicio5();
-//        ejercicio5.ejercicio();
-
+     
+        int opcion;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Ingrese el número del ejercicio a mostrar (1-6 ejercicios, 0 salir): ");
+            opcion = sc.nextInt();
+            switch (opcion){
+                case 1 -> {
+                    Ejercicio1 ejercicio1 = new Ejercicio1();
+                    ejercicio1.ejercicio();
+                    break;
+                }
+                case 2 -> {
+                    Ejercicio2 ejercicio2 = new Ejercicio2();
+                    ejercicio2.ejercicio();
+                    break;
+                }
+                case 3 -> {
+                    Ejercicio3 ejercicio3 = new Ejercicio3();
+                    ejercicio3.ejercicio();
+                    break;
+                }
+                case 4 -> {
+                    Ejercicio4 ejercicio4 = new Ejercicio4();
+                    ejercicio4.ejercicio();
+                    break;
+                }
+                case 5 -> {
+                    Ejercicio5 ejercicio5 = new Ejercicio5();
+                    ejercicio5.ejercicio();
+                    break;
+                }
+                case 6 -> {
+                    TeoriaEjercicios teoriaEjercicios = new TeoriaEjercicios();
+                    teoriaEjercicios.ejercicio();
+                    break;
+                }
+            }
+        } while (opcion!=0);
+        System.out.println("Gracias! Saludos!");
     }
-    
 }
